0c059388b88727d4c2948024bcb99f30bde13a78
alibazzi_cp <SND\alibazzi_cp>
2010-11-27 21:41:21 +0000

    Fix Namespaces issues

--- MyFlickr/MyFlickr.Rest/MyFlickr.Rest/Blogs.cs
+++ MyFlickr/MyFlickr.Rest/MyFlickr.Rest/Blogs.cs
@@ -3,77 +3,80 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Xml.Linq;
 
-/// <summary>
-/// represents a Collection of Blogs
-/// </summary>
-public class BlogsCollection : IEnumerable<Blog>
+namespace MyFlickr.Rest
 {
-    private XElement data;
-
-    internal BlogsCollection(XElement element)
-    {
-        this.data = element;
-        this.BlogsCount = element.Elements("blog").Count();
-    }
-
     /// <summary>
-    /// the number of blogs in this collection
+    /// represents a Collection of Blogs
     /// </summary>
-    public int BlogsCount { get; private set; }
-
-    /// <summary>
-    /// Blogs Objects
-    /// </summary>
-    public IEnumerable<Blog> Blogs
+    public class BlogsCollection : IEnumerable<Blog>
     {
-        get
+        private XElement data;
+
+        internal BlogsCollection(XElement element)
         {
-            return this.data.Elements("blog").Select(elm => new Blog(elm));
+            this.data = element;
+            this.BlogsCount = element.Elements("blog").Count();
+        }
+
+        /// <summary>
+        /// the number of blogs in this collection
+        /// </summary>
+        public int BlogsCount { get; private set; }
+
+        /// <summary>
+        /// Blogs Objects
+        /// </summary>
+        public IEnumerable<Blog> Blogs
+        {
+            get
+            {
+                return this.data.Elements("blog").Select(elm => new Blog(elm));
+            }
+        }
+
+        public IEnumerator<Blog> GetEnumerator()
+        {
+            foreach (var blog in this.Blogs)
+                yield return blog;
+        }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
         }
     }
 
-    public IEnumerator<Blog> GetEnumerator()
+    /// <summary>
+    /// Represents a blog
+    /// </summary>
+    public class Blog
     {
-        foreach (var blog in this.Blogs)
-            yield return blog;
+        internal Blog(XElement element)
+        {
+            this.ID = Int64.Parse(element.Attribute("id").Value);
+            this.URL = new Uri(element.Attribute("url").Value);
+            this.Name = element.Attribute("name").Value;
+            this.NeedsPassword = element.Attribute("needspassword").Value.ToBoolean();
+        }
+
+        /// <summary>
+        /// the ID of the Blog
+        /// </summary>
+        public Int64 ID { get; private set; }
+
+        /// <summary>
+        /// the URL that leads to the blog Service
+        /// </summary>
+        public Uri URL { get; private set; }
+
+        /// <summary>
+        /// determine whether the blog needs password
+        /// </summary>
+        public bool NeedsPassword { get; private set; }
+
+        /// <summary>
+        /// the name of the Blog Service
+        /// </summary>
+        public string Name { get; private set; }
     }
-
-    System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
-    {
-        return this.GetEnumerator();
-    }
-}
-
-/// <summary>
-/// Represents a blog
-/// </summary>
-public class Blog
-{
-    internal Blog(XElement element)
-    {
-        this.ID = Int64.Parse(element.Attribute("id").Value);
-        this.URL = new Uri(element.Attribute("url").Value);
-        this.Name = element.Attribute("name").Value;
-        this.NeedsPassword = element.Attribute("needspassword").Value.ToBoolean();
-    }
-
-    /// <summary>
-    /// the ID of the Blog
-    /// </summary>
-    public Int64 ID { get; private set; }
-
-    /// <summary>
-    /// the URL that leads to the blog Service
-    /// </summary>
-    public Uri URL { get; private set; }
-
-    /// <summary>
-    /// determine whether the blog needs password
-    /// </summary>
-    public bool NeedsPassword { get; private set; }
-
-    /// <summary>
-    /// the name of the Blog Service
-    /// </summary>
-    public string Name { get; private set; }
 }
--- MyFlickr/MyFlickr.Rest/MyFlickr.Rest/Contacts.cs
+++ MyFlickr/MyFlickr.Rest/MyFlickr.Rest/Contacts.cs
@@ -1,150 +1,153 @@
-﻿using System.Collections.Generic;
-using System.Xml.Linq;
-using System;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Xml.Linq;
 
-/// <summary>
-/// represents a list of contact
-/// </summary>
-public class ContactsList : IEnumerable<Contact>
+namespace MyFlickr.Rest
 {
-    private XElement data;
-
-    internal ContactsList(XElement element)
+    /// <summary>
+    /// represents a list of contact
+    /// </summary>
+    public class ContactsList : IEnumerable<Contact>
     {
-        this.data = element;
-        this.Page = int.Parse(element.Attribute("page").Value);
-        this.Pages = int.Parse(element.Attribute("pages").Value);
-        this.PerPage = int.Parse(element.Attribute("per_page").Value);
-        this.Total = int.Parse(element.Attribute("total").Value);
-    }
+        private XElement data;
 
-    /// <summary>
-    /// the page number
-    /// </summary>
-    public int Page { get; private set; }
-
-    /// <summary>
-    /// the available pages
-    /// </summary>
-    public int Pages { get; private set; }
-
-    /// <summary>
-    /// the number of contacts per page
-    /// </summary>
-    public int PerPage { get; private set; }
-
-    /// <summary>
-    /// the total number of contacts
-    /// </summary>
-    public int Total { get; private set; }
-
-    /// <summary>
-    /// the contacts objects
-    /// </summary>
-    public IEnumerable<Contact> Contacts
-    {
-        get
+        internal ContactsList(XElement element)
         {
-            return data.Elements("contact").Select(elm =>new Contact(elm));
+            this.data = element;
+            this.Page = int.Parse(element.Attribute("page").Value);
+            this.Pages = int.Parse(element.Attribute("pages").Value);
+            this.PerPage = int.Parse(element.Attribute("per_page").Value);
+            this.Total = int.Parse(element.Attribute("total").Value);
+        }
+
+        /// <summary>
+        /// the page number
+        /// </summary>
+        public int Page { get; private set; }
+
+        /// <summary>
+        /// the available pages
+        /// </summary>
+        public int Pages { get; private set; }
+
+        /// <summary>
+        /// the number of contacts per page
+        /// </summary>
+        public int PerPage { get; private set; }
+
+        /// <summary>
+        /// the total number of contacts
+        /// </summary>
+        public int Total { get; private set; }
+
+        /// <summary>
+        /// the contacts objects
+        /// </summary>
+        public IEnumerable<Contact> Contacts
+        {
+            get
+            {
+                return data.Elements("contact").Select(elm => new Contact(elm));
+            }
+        }
+
+        public IEnumerator<Contact> GetEnumerator()
+        {
+            foreach (var contact in this.Contacts)
+                yield return contact;
+        }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
         }
     }
 
-    public IEnumerator<Contact> GetEnumerator()
+    /// <summary>
+    /// Represents the Contact information
+    /// </summary>
+    public class Contact
     {
-        foreach (var contact in this.Contacts)
-            yield return contact;
+        internal Contact(XElement elm)
+        {
+            this.UserID = elm.Attribute("nsid").Value;
+            this.UserName = elm.Attribute("username").Value;
+            this.IconServer = int.Parse(elm.Attribute("iconserver").Value);
+            this.IsIgnored = elm.Attribute("ignored") != null ? new Nullable<bool>(elm.Attribute("ignored").Value.ToBoolean()) : null;
+            this.RealName = elm.Attribute("realname") != null ? elm.Attribute("realname").Value : null;
+            this.IsFriend = elm.Attribute("friend") != null ? new Nullable<bool>(elm.Attribute("friend").Value.ToBoolean()) : null;
+            this.IsFamily = elm.Attribute("family") != null ? new Nullable<bool>(elm.Attribute("family").Value.ToBoolean()) : null;
+            this.PathAlias = elm.Attribute("path_alias") != null ? elm.Attribute("path_alias").Value : null;
+            this.PhotosUploaded = elm.Attribute("photos_uploaded") != null ? new Nullable<int>(int.Parse(elm.Attribute("photos_uploaded").Value)) : null;
+        }
+
+        /// <summary>
+        /// the User ID of the Contact
+        /// </summary>
+        public string UserID { get; private set; }
+
+        /// <summary>
+        /// the real Name of the Contact , Could be Null
+        /// </summary>
+        public string RealName { get; private set; }
+
+        /// <summary>
+        /// the Name of the Contact
+        /// </summary>
+        public string UserName { get; private set; }
+
+        /// <summary>
+        /// is the contact marked as friend , could be Null
+        /// </summary>
+        public Nullable<bool> IsFriend { get; private set; }
+
+        /// <summary>
+        /// is the contact marked as family , could be Null
+        /// </summary>
+        public Nullable<bool> IsFamily { get; private set; }
+
+        /// <summary>
+        /// the number of icon server
+        /// </summary>
+        public int IconServer { get; private set; }
+
+        /// <summary>
+        /// the contact is ignored , Could Be Null
+        /// </summary>
+        public Nullable<bool> IsIgnored { get; private set; }
+
+        /// <summary>
+        ///the Path Alias (used when Generating Urls ) , Could be Empty when not set by the user
+        /// </summary>
+        public string PathAlias { get; private set; }
+
+        /// <summary>
+        /// the Number of photos Uploaded recently by the Contact , Could Be null
+        /// </summary>
+        public Nullable<int> PhotosUploaded { get; private set; }
     }
 
-    System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+    /// <summary>
+    /// The Filters that could be Applied on Contacts List when getting them
+    /// </summary>
+    public enum ContactFilter
     {
-        return this.GetEnumerator();
+        /// <summary>
+        /// the contact is Friend
+        /// </summary>
+        Friends = 0,
+        /// <summary>
+        /// the contact is Family
+        /// </summary>
+        Family = 1,
+        /// <summary>
+        /// the contact is both friend and family
+        /// </summary>
+        Both = 2,
+        /// <summary>
+        /// the contact is neither family nor friend
+        /// </summary>
+        Niether = 3
     }
-}
-
-/// <summary>
-/// Represents the Contact information
-/// </summary>
-public class Contact
-{
-    internal Contact(XElement elm)
-    {
-        this.UserID = elm.Attribute("nsid").Value;
-        this.UserName = elm.Attribute("username").Value;
-        this.IconServer = int.Parse(elm.Attribute("iconserver").Value);
-        this.IsIgnored = elm.Attribute("ignored") != null ? new Nullable<bool>(elm.Attribute("ignored").Value.ToBoolean()) : null;
-        this.RealName = elm.Attribute("realname") != null ? elm.Attribute("realname").Value : null;
-        this.IsFriend = elm.Attribute("friend") != null ? new Nullable<bool>(elm.Attribute("friend").Value.ToBoolean()) : null;
-        this.IsFamily = elm.Attribute("family") != null ? new Nullable<bool>(elm.Attribute("family").Value.ToBoolean()) : null;
-        this.PathAlias = elm.Attribute("path_alias") != null ? elm.Attribute("path_alias").Value : null;
-        this.PhotosUploaded = elm.Attribute("photos_uploaded") != null ? new Nullable<int>(int.Parse(elm.Attribute("photos_uploaded").Value)) : null;
-    }
-
-    /// <summary>
-    /// the User ID of the Contact
-    /// </summary>
-    public string UserID { get; private set; }
-
-    /// <summary>
-    /// the real Name of the Contact , Could be Null
-    /// </summary>
-    public string RealName { get; private set; }
-
-    /// <summary>
-    /// the Name of the Contact
-    /// </summary>
-    public string UserName { get; private set; }
-
-    /// <summary>
-    /// is the contact marked as friend , could be Null
-    /// </summary>
-    public Nullable<bool> IsFriend { get; private set; }
-
-    /// <summary>
-    /// is the contact marked as family , could be Null
-    /// </summary>
-    public Nullable<bool> IsFamily { get; private set; }
-
-    /// <summary>
-    /// the number of icon server
-    /// </summary>
-    public int IconServer { get; private set; }
-
-    /// <summary>
-    /// the contact is ignored , Could Be Null
-    /// </summary>
-    public Nullable<bool> IsIgnored { get; private set; }
-
-    /// <summary>
-    ///the Path Alias (used when Generating Urls ) , Could be Empty when not set by the user
-    /// </summary>
-    public string PathAlias { get; private set; }
-
-    /// <summary>
-    /// the Number of photos Uploaded recently by the Contact , Could Be null
-    /// </summary>
-    public Nullable<int> PhotosUploaded { get; private set; }
-}
-
-/// <summary>
-/// The Filters that could be Applied on Contacts List when getting them
-/// </summary>
-public enum ContactFilter
-{
-    /// <summary>
-    /// the contact is Friend
-    /// </summary>
-    Friends = 0,
-    /// <summary>
-    /// the contact is Family
-    /// </summary>
-    Family = 1,
-    /// <summary>
-    /// the contact is both friend and family
-    /// </summary>
-    Both = 2,
-    /// <summary>
-    /// the contact is neither family nor friend
-    /// </summary>
-    Niether = 3
 }
